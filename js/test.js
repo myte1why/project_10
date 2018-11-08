b862bc647fccfd9d040e1bc9d3fdffd7f8ac5253
Murat Hasdemir <murathasdemir0@gmail.com>
2018-11-08 13:11:05 +0300

    Update js/test.js

--- js/test.js
+++ js/test.js
@@ -1,28 +1,28 @@
 $(document).ready(function() {
-   var apioptList = [{t : "iron man", y : "",plot :"short",r :"json"},
-                    {t : "iron man 2", y : "",plot :"short",r :"json"},
-                    {t : "iron man 3", y : "",plot :"short",r :"json"},
-                    {t : "The Incredible Hulk", y : "",plot :"short",r :"json"},
-                    {t : "Thor", y : "",plot :"short",r :"json"},
-                    {t : "Captain America: The First Avenger", y : "",plot :"short",r :"json"},
-                    {t : "The Avengers", y : "",plot :"short",r :"json"},
-                    {t : "Thor: The Dark World", y : "",plot :"short",r :"json"},
-                    {t : "Captain America: The Winter Soldier", y : "",plot :"short",r :"json"},
-                    {t : "Guardians of the Galaxy", y : "",plot :"short",r :"json"},
-                    {t : "Avengers: Age of Ultron", y : "",plot :"short",r :"json"},
-                    {t : "Ant-Man", y : "",plot :"short",r :"json"},
-                    {t : "Captain America: Civil War", y : "",plot :"short",r :"json"},
-                    {t : "Doctor Strange", y : "",plot :"short",r :"json"},
-                    {t : "Guardians of the Galaxy Vol. 2", y : "",plot :"short",r :"json"},
-                    {t : "Spider-Man: Homecoming", y : "",plot :"short",r :"json"},
-                    {t : "Thor: Ragnarok", y : "",plot :"short",r :"json"},
-                    {t : "Black Panther", y : "",plot :"short",r :"json"},
-                    {t : "Avengers: Infinity War", y : "",plot :"short",r :"json"}
+   var apioptList = [{t : "iron man", y : "",plot :"short",r :"json",apikey:"8d84c350"},
+                    {t : "iron man 2", y : "",plot :"short",r :"json",apikey:"8d84c350"},
+                    {t : "iron man 3", y : "",plot :"short",r :"json",apikey:"8d84c350"},
+                    {t : "The Incredible Hulk", y : "",plot :"short",r :"json",apikey:"8d84c350"},
+                    {t : "Thor", y : "",plot :"short",r :"json",apikey:"8d84c350"},
+                    {t : "Captain America: The First Avenger", y : "",plot :"short",r :"json",apikey:"8d84c350"},
+                    {t : "The Avengers", y : "",plot :"short",r :"json",apikey:"8d84c350"},
+                    {t : "Thor: The Dark World", y : "",plot :"short",r :"json",apikey:"8d84c350"},
+                    {t : "Captain America: The Winter Soldier", y : "",plot :"short",r :"json",apikey:"8d84c350"},
+                    {t : "Guardians of the Galaxy", y : "",plot :"short",r :"json",apikey:"8d84c350"},
+                    {t : "Avengers: Age of Ultron", y : "",plot :"short",r :"json",apikey:"8d84c350"},
+                    {t : "Ant-Man", y : "",plot :"short",r :"json",apikey:"8d84c350"},
+                    {t : "Captain America: Civil War", y : "",plot :"short",r :"json",apikey:"8d84c350"},
+                    {t : "Doctor Strange", y : "",plot :"short",r :"json",apikey:"8d84c350"},
+                    {t : "Guardians of the Galaxy Vol. 2", y : "",plot :"short",r :"json",apikey:"8d84c350"},
+                    {t : "Spider-Man: Homecoming", y : "",plot :"short",r :"json",apikey:"8d84c350"},
+                    {t : "Thor: Ragnarok", y : "",plot :"short",r :"json",apikey:"8d84c350"},
+                    {t : "Black Panther", y : "",plot :"short",r :"json",apikey:"8d84c350"},
+                    {t : "Avengers: Infinity War", y : "",plot :"short",r :"json",apikey:"8d84c350"}
                     ];
 
 
 
-    var apilink     = 'http://www.omdbapi.com/?apikey=8d84c350&';
+    var apilink     = 'http://www.omdbapi.com/';
          function apifunction(data){
             var item = "<h2>" + data["Title"] + "</h2>";
             item += "<h3>DIRECTOR :" +data["Director"]+"</h3>";
